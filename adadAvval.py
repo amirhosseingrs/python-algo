c9e3eb9225d5472edb75e9272b5899ce3d3f3631
Amir Hossein Garousi <amirhosseingarousi@gmail.com>
2022-01-21 05:53:28 +0330

    First Commit

---
+++ adadAvval.py
@@ -0,0 +1,13 @@
+x = int(input())
+y = int(input())
+i = x
+primes = []
+while i <= y : 
+    isPrime = True
+    for x in range(2,i):
+        if i%x == 0 : 
+            isPrime = False 
+    if i == 1 : isPrime = False
+    if isPrime == True : primes.append(i)
+    i += 1
+[print(x) for x in primes]    
